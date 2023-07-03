7346ca3576e787c7bf7a36f289e62e16cc6f4e19
Hilal Abaci <hilalabaci55@gmail.com>
2023-07-03 14:23:20 +0100

    fixxed mobile view

--- src/components/boards/board-menu/styles.jsx
+++ src/components/boards/board-menu/styles.jsx
@@ -11,6 +11,7 @@ export const Container = styled.div`
 
   @media only screen and (max-width: 768px) {
     align-items: center;
+    width: 20px;
   }
 `;
 export const AddBoardWrapper = styled.div`
